da5ef003d43e9302c747e14dd6b6b9e9b31c05dd
sunnyleeee <tjswo98sj06@gmail.com>
2020-09-26 04:48:30 +0900

    update java-project 대문자 행별로 나열하기

--- src/java_project/ClassExam.java
+++ src/java_project/ClassExam.java
@@ -2,8 +2,17 @@ package java_project;
 
 public class ClassExam {
 	public static void main(String[] args ){
+		int i,j;
+		char c = 'A';
 		
-		System.out.println("hello git");
-	
+		for(i=1; i<=26; i++)
+		{
+			c='A';
+			for(j=1; j<=i; j++)
+				System.out.print(c++ +"  ");
+				
+			System.out.println();
+		}
+
 	}
 }
